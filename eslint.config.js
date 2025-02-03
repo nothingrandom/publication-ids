37ef7083021d8d95feafdb6e7c6aef44c390be50
Benjamin Grant <benjamin@nothingrandom.com>
2025-02-03 22:00:28 +0000

    feat: setup

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintImport from 'eslint-plugin-import';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default tseslint.config(
+  {
+    ignores: ['**/tests/**'],
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    settings: {
+      'import/resolver': {
+        typescript: true,
+      },
+    },
+  },
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  eslintImport.flatConfigs.recommended,
+  eslintImport.flatConfigs.typescript,
+  stylistic.configs.customize({
+    indent: 2,
+    quotes: 'single',
+    semi: true,
+    jsx: true,
+    braceStyle: '1tbs',
+  }),
+);
